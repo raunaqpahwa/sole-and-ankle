5a32b6cfc6aa1ddc51bb6c12f576075223ee98e4
Raunaq Pahwa <raunaqpahwa@gmail.com>
2024-01-12 22:11:09 -0500

    Completed CSS changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
-import { COLORS, WEIGHTS } from '../../constants';
-import Logo from '../Logo';
-import SuperHeader from '../SuperHeader';
+import { COLORS, WEIGHTS } from "../../constants";
+import Logo from "../Logo";
+import SuperHeader from "../SuperHeader";
 
 const Header = () => {
   // Our site features two visual headers, but they should be
@@ -29,9 +29,15 @@ const Header = () => {
 const MainHeader = styled.div`
   padding: 0 32px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  display: flex;
+  align-items: baseline;
+  padding: 24px 32px;
 `;
 
-const Nav = styled.nav``;
+const Nav = styled.nav`
+  margin: auto;
+  padding-top: 3px;
+`;
 
 const NavLink = styled.a`
   font-size: 1.125rem;
@@ -39,7 +45,7 @@ const NavLink = styled.a`
   text-decoration: none;
   color: ${COLORS.gray[900]};
   font-weight: ${WEIGHTS.medium};
-
+  margin-left: 48px;
   &:first-of-type {
     color: ${COLORS.secondary};
   }
--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
-import { COLORS, WEIGHTS } from '../../constants';
-import { formatPrice, pluralize, isNewShoe } from '../../utils';
-import Spacer from '../Spacer';
+import { COLORS, WEIGHTS } from "../../constants";
+import { formatPrice, pluralize, isNewShoe } from "../../utils";
+import Spacer from "../Spacer";
 
 const ShoeCard = ({
   slug,
@@ -36,15 +36,24 @@ const ShoeCard = ({
       <Wrapper>
         <ImageWrapper>
           <Image alt="" src={imageSrc} />
+          {variant !== "default" && (
+            <Banner variant={variant}>
+              {variant === "new-release" ? "Just Released!" : "Sale"}
+            </Banner>
+          )}
         </ImageWrapper>
+
         <Spacer size={12} />
-        <Row>
+        <StyledRow>
           <Name>{name}</Name>
-          <Price>{formatPrice(price)}</Price>
-        </Row>
-        <Row>
-          <ColorInfo>{pluralize('Color', numOfColors)}</ColorInfo>
-        </Row>
+          <Price strike={variant === "on-sale"}>{formatPrice(price)}</Price>
+        </StyledRow>
+        <StyledRow>
+          <ColorInfo>{pluralize("Color", numOfColors)}</ColorInfo>
+          {variant === "on-sale" && (
+            <SalePrice>{formatPrice(salePrice)}</SalePrice>
+          )}
+        </StyledRow>
       </Wrapper>
     </Link>
   );
@@ -61,10 +70,18 @@ const ImageWrapper = styled.div`
   position: relative;
 `;
 
-const Image = styled.img``;
+const Image = styled.img`
+  border-radius: 16px 16px 4px 4px;
+  max-width: 100%;
+`;
 
 const Row = styled.div`
   font-size: 1rem;
+`;
+
+const StyledRow = styled(Row)`
+  display: flex;
+  justify-content: space-between;
 `;
 
 const Name = styled.h3`
@@ -72,7 +89,11 @@ const Name = styled.h3`
   color: ${COLORS.gray[900]};
 `;
 
-const Price = styled.span``;
+const Price = styled.span`
+  text-decoration: ${(props) => props.strike && "line-through"};
+  color: ${(props) => (props.strike ? COLORS.gray[700] : COLORS.gray[900])};
+  margin-top: -4px;
+`;
 
 const ColorInfo = styled.p`
   color: ${COLORS.gray[700]};
@@ -81,6 +102,21 @@ const ColorInfo = styled.p`
 const SalePrice = styled.span`
   font-weight: ${WEIGHTS.medium};
   color: ${COLORS.primary};
+  margin-top: -4px;
+`;
+
+const Banner = styled.span`
+  position: absolute;
+  top: 16px;
+  right: 0px;
+  border-radius: 2px;
+  font-weight: 700;
+  font-size: ${14 / 16}rem;
+  margin-right: -4px;
+  padding: 10px;
+  color: white;
+  background-color: ${(props) =>
+    props.variant === "on-sale" ? COLORS.primary : COLORS.secondary};
 `;
 
 export default ShoeCard;
